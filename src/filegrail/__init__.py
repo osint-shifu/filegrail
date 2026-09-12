2ff056251f2c69465a7aa77a2511a9c3cb340c0b
Oryon <0ryon@riseup.net>
2026-09-12 15:57:46 +0200

    Release 0.9.0

--- src/filegrail/__init__.py
+++ src/filegrail/__init__.py
@@ -1,6 +1,6 @@
 """filegrail - reconstruct where files came from, after the fact."""
 
-__version__ = "0.8.2"
+__version__ = "0.9.0"
 
 AUTHOR = "osint-shifu"
 REPOSITORY = "https://github.com/osint-shifu/filegrail"
