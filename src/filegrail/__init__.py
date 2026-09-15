f6f29515a7370dcc07ca229a171d43b54904ce67
Oryon <0ryon@riseup.net>
2026-09-16 00:03:36 +0200

    Release 0.15.0

--- src/filegrail/__init__.py
+++ src/filegrail/__init__.py
@@ -1,6 +1,6 @@
 """filegrail - reconstruct where files came from, after the fact."""
 
-__version__ = "0.14.0"
+__version__ = "0.15.0"
 
 AUTHOR = "osint-shifu"
 REPOSITORY = "https://github.com/osint-shifu/filegrail"
